f8e2a7e2105a69f343303060036fd553625b187c
lizbur10 <liz.burton147@gmail.com>
2018-03-27 15:50:57 -0400

    updated site index and logged in home pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     def logged_in
         @bander = Bander.find(1) # placeholder for login functionality
-        @reports = Report.all    # placeholder for nested resource
+        @reports = Report.all    # placeholder for scoped resource - draft vs. posted
     end
 end
