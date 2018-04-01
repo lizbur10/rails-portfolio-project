11e40bbe2726a91d3e78af9c8cf6cb96a48ad9ae
lizbur10 <liz.burton147@gmail.com>
2018-04-01 19:01:38 -0400

    ironed out some wrinkles with the birds_of_species_attributes custom writer

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,20 +10,21 @@ class Report < ApplicationRecord
     end
 
     def birds_of_species_attributes=(birds_of_species_attributes)
-        if !self.species.find_by(:name => birds_of_species_attributes[:"#{birds_of_species_attributes.length-1}"][:species_attributes][:name])
+        binding.pry
+        if birds_of_species_attributes[:"#{birds_of_species_attributes.length-1}"][:species_attributes][:name] != "" && !self.species.find_by(:name => birds_of_species_attributes[:"#{birds_of_species_attributes.length-1}"][:species_attributes][:name])
             self.birds_of_species.build(birds_of_species_attributes[:"#{birds_of_species_attributes.length-1}"])
         end
         self.save
         birds_of_species_attributes.each do |bosa|
-            this_species_name = bosa[1]["species_attributes"]["name"]
-            this_species = self.species.find_by(:name => this_species_name)
-            this_bird_of_species = this_species.birds_of_species.first
-            if bosa[1]["number_banded"].to_i != this_bird_of_species.number_banded
-                this_bird_of_species.number_banded = bosa[1]["number_banded"].to_i
-                this_bird_of_species.save
+            if bosa[1]["species_attributes"]["name"] != ""
+                this_species_name = bosa[1]["species_attributes"]["name"]
+                this_bird_of_species = self.species.find_by(:name => this_species_name).birds_of_species.first
+                if bosa[1]["number_banded"].to_i != this_bird_of_species.number_banded
+                    this_bird_of_species.number_banded = bosa[1]["number_banded"].to_i
+                    this_bird_of_species.save
+                end
             end
         end
-        binding.pry
     end
 
 end
