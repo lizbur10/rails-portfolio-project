23cb069f45c09951ce6d3833d3e8cde77f552126
lizbur10 <liz.burton147@gmail.com>
2018-05-27 16:20:33 -0400

    started to code AJAX post request for report

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,7 +1,30 @@
-$(document).ready(function() {
+$(function() {
 
     $("input[value='Continue']").on("click", function(e) {
-        alert("You clicked this button");
+        data = {
+            'authenticity_token': $("input[name = 'authenticity_token']").val(),
+            'report': {
+                'date': "2018-05-27", 
+                'birds_of_species_attributes': {
+                    '0': {
+                        'bander_id':= "7", 
+                        'species_attributes': {
+                            'code': $("#report_birds_of_species_attributes_0_species_attributes_code").val(), 
+                            'name': $("#report_birds_of_species_attributes_0_species_attributes_name").val()
+                        }, 
+                        'number_banded': $("#report_birds_of_species_attributes_0_number_banded").val()
+                    }
+                }
+            }
+        }
+        $.ajax({
+            method: "POST",
+            url: this.form.action,
+            data: data, 
+            success: function(response) {
+                debugger;
+            }
+        })
         e.preventDefault();
     });
 
