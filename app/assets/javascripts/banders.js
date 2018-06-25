81f1cb08d3f2601e5f7a1256aa043afe3ea799d9
lizbur10 <liz.burton147@gmail.com>
2018-06-25 15:42:05 -0400

    refactored/generalized ajax click events on bander/show page

--- app/assets/javascripts/banders.js
+++ app/assets/javascripts/banders.js
@@ -1,14 +1,6 @@
 $(function() {
 
-    $("a.load_reports").on("click", function(e){
-        $.ajax({
-            url: this.href,
-            dataType: 'script'
-        })
-        e.preventDefault();
-    })
-
-    $("div.posted_reports").on("click", "a.load_report", function(e){
+    $("div.posted_reports").on("click", "a.load_target", function(e){
         $.ajax({
             url: this.href,
             dataType: 'script'
