3f51a503577cc08f24e24802d0313413973cf44f
lizbur10 <liz.burton147@gmail.com>
2018-07-02 10:49:05 -0400

    switched reports#show to render json; started to code show view using json

--- app/assets/javascripts/banders.js
+++ app/assets/javascripts/banders.js
@@ -11,12 +11,16 @@ $(function() {
                 report_urls.push($(this).attr("href")) 
             })
         }).then(function(){
+            // NEED TO SWITCH THIS AND THE 'NEXT' EVENT HANDLER TO JSON
             url = report_urls.shift();
-            $.ajax({
-                url: url, // reports/:id
-                dataType: 'script' // views/reports/show.js.erb -> _posted_report
-            })
-    
+            // $.ajax({
+            //     url: url, // reports/:id
+            //     dataType: 'script' // views/reports/show.js.erb -> _posted_report
+            // })
+            $.get(url).success(function(json){ // json is what is returned 
+                report = new Report (json["date"], json["content"], json["birds_of_species"]);
+                $('.posted_report_view').html(`<h1>Report for: ${(report.formatDate())}</h1>`);
+            })                
         })
         e.preventDefault();
     })
@@ -24,6 +28,7 @@ $(function() {
     // CLICK EVENT FOR 'NEXT' LINK
     $("div.posted_reports").on("click", ".js-next", function(e){
 
+        // !!! SWITCH TO JSON
         // ADD IF STATEMENT TO CONTROL FOR LAST REPORT
         let new_url = report_urls.shift();
         $.ajax({
@@ -34,5 +39,21 @@ $(function() {
 
     })
 
+
+    class Report {
+        constructor(date, content, birds_of_species) {
+            this.date = date;
+            this.content = content;
+            this.birds_of_species = birds_of_species;
+        }
+
+        formatDate() {
+            return $.format.date(this.date, "MMMdd");
+        }
+
+        // renderReport() {
+
+        // }
+    }
 });
 
